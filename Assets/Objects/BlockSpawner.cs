6d80fa5557723803aa194ac9a7178ef91b66ae20
hellohi <nampham.ptn.2006@gmail.com>
2026-05-07 21:54:11 +0700

    add spawning cooldown

--- Assets/Objects/BlockSpawner.cs
+++ Assets/Objects/BlockSpawner.cs
@@ -5,37 +5,47 @@ using UnityEngine;
 public class BlockSpawner : MonoBehaviour
 {
     [SerializeField] private Player player;
-
     [SerializeField] private Block blockPrefab;
-
     [SerializeField] private List<Sprite> sprites;
+    
+    [SerializeField] private float spawnCooldown = 0.5f; // Cooldown duration in seconds
+    private float lastSpawnTime = -Mathf.Infinity;
 
     private bool isFirstSpawn = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        player.onHighCollision += spawnBlock;
-        bool isBase = true;
-        spawnBlock(new Vector2(player.transform.position.x, -2.0f));
+        player.onHighCollision += SpawnBlock;
+        DoSpawn(new Vector2(player.transform.position.x, -2.0f));
         isFirstSpawn = false;
+        lastSpawnTime = Time.time; 
     }
 
-    void spawnBlock(Vector2 pos)
+    void SpawnBlock(Vector2 pos)
+    {
+        //check if cooldown has elapsed
+        //TODO: implement coroutine for better responsiveness if needed
+        
+        if (Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            DoSpawn(pos);
+            lastSpawnTime = Time.time; 
+        }
+    }
+
+    void DoSpawn(Vector2 pos)
     {
         var block = Instantiate(
             blockPrefab,
-            new Vector3(pos.x, isFirstSpawn? pos.y : transform.position.y, 0f),
+            new Vector3(pos.x, isFirstSpawn ? pos.y : transform.position.y, 0f),
             Quaternion.identity
         );
         block.gameObject.layer = LayerMask.NameToLayer("Ground");
-        block.sr.sprite = sprites[0];//Random.Range(0, sprites.Count)];
+        block.sr.sprite = sprites[0];
         block.isKinematic = isFirstSpawn;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
     }
 }
